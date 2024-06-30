903d217d89528640eae9434e5f2d122190fc8ab3
SeungRyeolBaek <bsr20030507@g.skku.edu>
2024-07-01 00:57:24 +0900

    feat: LinkForm, CardList에 맞추어 folder 페이지 업데이트

--- pages/folder/[[...folderId]]/index.tsx
+++ pages/folder/[[...folderId]]/index.tsx
@@ -12,39 +12,54 @@ import { useRouter } from "next/router";
 import { useEffect, useMemo } from "react";
 
 const FolderPage = () => {
-	const router = useRouter();
-	const { folderId } = router.query;
-	const currentFolderId = useMemo(() => {
-	  if (router.isReady) {
-		return folderId?.[0] ? parseInt(folderId?.[0]) : ALL_LINKS_ID;
-	  }
-	  return undefined;
-	}, [router.isReady, folderId]);
-	const { data: folders } = useGetFolders();
-	const { data: links, loading } = useGetLinks(currentFolderId);
-	const { searchValue, handleChange, handleCloseClick, result } = useSearchLink(links);
-	const { ref, isIntersecting } = useIntersectionObserver<HTMLDivElement>();
-  
-	useEffect(() => {
-	  const accessToken = localStorage.getItem("accessToken");
-	  if (!accessToken) {
-		router.replace(ROUTE.로그인);
-	  }
-	}, [router]);
-  
-	return (
-	  <Layout isSticky={false} footerRef={ref}>
-		<FolderLayout
-		  linkForm={<LinkForm hideFixedLinkForm={isIntersecting} />}
-		  searchBar={
-			<SearchBar value={searchValue} onChange={handleChange} onCloseClick={handleCloseClick} />
-		  }
-		  folderToolBar={<FolderToolBar folders={folders} selectedFolderId={currentFolderId} />}
-		  cardList={loading ? null : <CardList links={result} />}
-		/>
-	  </Layout>
-	);
-  };
-  
-  export default FolderPage;
-  
+  const router = useRouter();
+  const { folderId } = router.query;
+  const currentFolderId = useMemo(() => {
+    if (router.isReady) {
+      return folderId?.[0] ? parseInt(folderId?.[0]) : ALL_LINKS_ID;
+    }
+    return undefined;
+  }, [router.isReady, folderId]);
+  const { data: folders } = useGetFolders();
+  const { data: links, loading } = useGetLinks(currentFolderId);
+  const { searchValue, handleChange, handleCloseClick, result } =
+    useSearchLink(links);
+  const { ref, isIntersecting } = useIntersectionObserver<HTMLDivElement>();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      router.replace(ROUTE.로그인);
+    }
+  }, [router]);
+
+  return (
+    <Layout isSticky={false} footerRef={ref}>
+      <FolderLayout
+        linkForm={
+          <LinkForm
+            hideFixedLinkForm={isIntersecting}
+            currentFolderId={currentFolderId}
+          />
+        }
+        searchBar={
+          <SearchBar
+            value={searchValue}
+            onChange={handleChange}
+            onCloseClick={handleCloseClick}
+          />
+        }
+        folderToolBar={
+          <FolderToolBar folders={folders} selectedFolderId={currentFolderId} />
+        }
+        cardList={
+          loading ? null : (
+            <CardList links={result} currentFolderId={currentFolderId} />
+          )
+        }
+      />
+    </Layout>
+  );
+};
+
+export default FolderPage;
